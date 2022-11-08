da2d9a213f0604a62485ebbab79255809361abcf
karfaisarra <karfaisarra@gmail.com>
2022-11-08 14:58:23 +0100

    add Axios

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+const {createApp} = Vue
+
+createApp({
+    data(){
+        return{
+            emailArray: []
+        }
+    },
+    mounted(){
+        for (let i = 1; i <= 10; i++) {
+            axios
+                .get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then(response =>{
+                    //console.log(response.data.response);
+                    this.emailArray.push(response.data.response)
+                })
+        }
+    }
+}).mount('#app')
